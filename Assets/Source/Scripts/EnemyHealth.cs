c943e759456bf57b208a92589c75517a60e93c45
MrHellHound <Spirin.Andrew1@mail.ru>
2025-03-12 18:06:25 +0300

    All levels done
    All levels done

--- Assets/Source/Scripts/EnemyHealth.cs
+++ Assets/Source/Scripts/EnemyHealth.cs
@@ -1,21 +1,39 @@
 using System;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class EnemyHealth : MonoBehaviour
 {
+    [SerializeField] private int healthMax;
+    [SerializeField] private int randomAmount;
+    [SerializeField] private int currentHealth;
+    [SerializeField] private GameObject enemyColor;
+    
     [SerializeField] Slider healthBar;
-    
-    [SerializeField] int healthMax = 100;
-    [SerializeField] int currentHealth;
-    
     [SerializeField] int damage = 50;
 
     private EnemyCounter _enemyCounter;
 
+    readonly Color _red = new Color(255f, 0f, 0f, 255f);
+    readonly Color _orange = new Color(255f, 147f, 0f, 255f);
+
     private void Awake()
     {
         _enemyCounter = GameObject.FindWithTag("GameLogic").GetComponent<EnemyCounter>();
+        
+        randomAmount = Random.Range(1, 3);
+        
+        if (randomAmount == 1)
+        {
+            healthMax = 100;
+            enemyColor.GetComponent<SpriteRenderer>().color = _red;
+        }
+        else if (randomAmount == 2)
+        {
+            healthMax = 200;
+            enemyColor.GetComponent<SpriteRenderer>().color = _orange;
+        }
         
         currentHealth = healthMax;
         
