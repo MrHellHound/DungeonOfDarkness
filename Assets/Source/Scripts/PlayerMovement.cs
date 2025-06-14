f27d6eb661915a27d09afe4f2bc1968145ac9a3d
MrHellHound <Spirin.Andrew1@mail.ru>
2025-06-15 00:18:07 +0300

    New character + lighter locations
    New character + lighter locations

--- Assets/Source/Scripts/PlayerMovement.cs
+++ Assets/Source/Scripts/PlayerMovement.cs
@@ -1,12 +1,13 @@
-using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float walkSpeed = 6f;
-    
     private Rigidbody2D _rb;
+    
+    [SerializeField] private GameObject playerModel;
+    [SerializeField] private GameObject gun;
 
     private void Awake()
     {
@@ -19,6 +20,17 @@ public class PlayerMovement : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         
         _rb.velocity = new Vector2(horizontal, vertical) * walkSpeed;
+
+        if (_rb.velocity.x < 0)
+        {
+            playerModel.transform.localScale = new Vector3(-1f, 1f, 1f);
+            //gun.transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (_rb.velocity.x > 0)
+        {
+            playerModel.transform.localScale = new Vector3(1f, 1f, 1f);
+            //gun.transform.localScale = new Vector3(1f, 1f, 1f);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
